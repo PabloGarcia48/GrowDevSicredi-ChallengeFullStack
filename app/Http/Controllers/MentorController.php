779d54f7494d07aa7fa27a6e922c47a84d6504df
Pablo Garcia <pablorobertodev@gmail.com>
2024-08-26 17:22:40 -0300

    First commit

---
+++ app/Http/Controllers/MentorController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mentor;
+use Illuminate\Http\Request;
+
+class MentorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $mentors = Mentor::all();
+        return $mentors;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mentor $mentor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Mentor $mentor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mentor $mentor)
+    {
+        //
+    }
+}
